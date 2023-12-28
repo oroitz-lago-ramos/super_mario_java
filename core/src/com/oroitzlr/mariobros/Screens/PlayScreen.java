085d5a12b442ac5c1806a94af3ed9d766660409d
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-12-28 15:23:46 +0100

    Ajout de la map niveau1 et affichage dans le jeu, la camera avance au click

--- core/src/com/oroitzlr/mariobros/Screens/PlayScreen.java
+++ core/src/com/oroitzlr/mariobros/Screens/PlayScreen.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -19,11 +22,19 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gamecam;
     private Viewport gamePort;
     private Hud hud;
+
+    private TmxMapLoader maploader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
     public PlayScreen(MarioBros game){
         this.game = game;
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_HEIGHT,gamecam);
         hud = new Hud(game.batch);
+        maploader = new TmxMapLoader();
+        map = maploader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
 
 
     }
@@ -32,10 +43,25 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt){
+        if (Gdx.input.isTouched())
+        {
+            gamecam.position.x += 100 * dt;
+        }
+
+    }
+    public void update(float dt){
+        handleInput(dt);
+        gamecam.update();
+        renderer.setView(gamecam);
+
+    }
     @Override
     public void render(float delta) {
+        update(delta);
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.render();
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
