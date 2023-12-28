c708ad11c95dbea38ed18e8f0fe96be5de37eecd
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2023-12-28 01:14:41 +0100

    setup project and bult first screen with an HUD and creation of Tiled map

---
+++ core/src/com/oroitzlr/mariobros/MarioBros.java
@@ -0,0 +1,29 @@
+package com.oroitzlr.mariobros;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.oroitzlr.mariobros.Screens.PlayScreen;
+
+public class MarioBros extends Game {
+	public SpriteBatch batch;
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
